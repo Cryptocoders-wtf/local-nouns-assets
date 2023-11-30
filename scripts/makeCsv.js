06ff000b517d4f15bff279c163b95512a20498b8
eibakatsu <eiba.katsu@gmail.com>
2023-11-30 11:18:06 +0900

    ご当地パーツの説明書き入力用のCSV

---
+++ scripts/makeCsv.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+const pngFiles = (dir) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        console.log(err);
+        return reject([]);
+      }
+      const pngs = files.filter(file => /png$/.test(file));
+      resolve(pngs);
+      // console.log(dir, pngs);
+    });
+  });
+};
+
+const imageUrl = 'https://raw.githubusercontent.com/EibaKatsu/local-nouns-assets/main/images';
+const image = (file, baseDir, dir) => {
+  return `${imageUrl}/${baseDir}/${dir}/${file}`;
+}
+const readDir = async (baseDir) => {
+  // console.log("# " + baseDir);
+  const assetDir = './images/' + baseDir;
+
+  await (() => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(assetDir, async (err, dirs) => {
+        for (let i = 0; i < dirs.length; i++) {
+          const dir = dirs[i];
+          if (dir.match(/^\d{2}-/)) {
+            const prefDir = assetDir + "/" + dir;
+            const pngs = await pngFiles(prefDir);
+            // console.log("## " + dir)
+            pngs.map(png => {
+              const line = `${baseDir},${dir},${png},${image(png, baseDir, dir)}`;
+              console.log(line);
+              // console.log(" - " + png + " " + image(png, baseDir, dir));
+            });
+          }
+        }
+        resolve()
+      });
+    })
+  })()
+}
+
+const main = async () => {
+  await readDir("2-accessories");
+  await readDir("3-heads");
+};
+
+main();
